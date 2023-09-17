e1cb3eb7dd073001aee1ad6a8fceab7d951f4dca
Franco Carreira <carreirafranco@gmail.com>
2023-09-17 17:43:05 -0300

    for with break

--- CsharpProjects/For/Program.cs
+++ CsharpProjects/For/Program.cs
@@ -1,4 +1,9 @@
-﻿for (int i = 0; i < 10; i++)
+﻿// for (int i = 0; i < 10; i++)
+// {
+//     Console.WriteLine(i);
+// }
+for (int i = 0; i < 10; i++)
 {
     Console.WriteLine(i);
+    if (i == 7) break;
 }
