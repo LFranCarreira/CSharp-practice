fc937a6973ed6bcc2188b35ff36e07b26167dbe7
Franco Carreira <carreirafranco@gmail.com>
2023-09-17 18:28:17 -0300

    reassign

--- CsharpProjects/For/Program.cs
+++ CsharpProjects/For/Program.cs
@@ -8,7 +8,7 @@
 //     if (i == 7) break;
 // }
 string[] names = { "Alex", "Eddie", "David", "Michael" };
-for (int i = names.Length - 1; i >= 0; i--)
-{
-    Console.WriteLine(names[i]);
-}
+for (int i = 0; i < names.Length; i++)
+    if (names[i] == "David") names[i] = "Sammy";
+
+foreach (var name in names) Console.WriteLine(name);
