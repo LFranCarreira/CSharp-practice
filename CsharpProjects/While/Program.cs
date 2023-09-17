29ab331a4706f4ce7d13a16cbce270f895aa6a0e
Franco Carreira <carreirafranco@gmail.com>
2023-09-17 19:51:57 -0300

    while

--- CsharpProjects/While/Program.cs
+++ CsharpProjects/While/Program.cs
@@ -1,16 +1,26 @@
-﻿Random dice = new Random();
-int heroeHealth = 10;
-int villianHealth = 10;
+﻿// Random dice = new Random();
+// int heroeHealth = 10;
+// int villianHealth = 10;
+// do
+// {
+//     int roll = dice.Next(1,11);
+//     villianHealth-=roll;
+//     Console.WriteLine($"Monster was damaged and lost {roll} health and now has {villianHealth} health.");
+    
+//     if(villianHealth <= 0)continue;
+
+//     roll = dice.Next(1,11);
+//     heroeHealth -= roll;
+//     Console.WriteLine($"Hero was damaged and lost {roll} health and now has {heroeHealth} health.");
+// } while (heroeHealth > 0 && villianHealth > 0);
+// Console.WriteLine(heroeHealth > villianHealth ? "Hero Wins!" : "Monster Wins!");
+string? readResult;
+Console.WriteLine("Enter a string:");
 do
 {
-    int roll = dice.Next(1,11);
-    villianHealth-=roll;
-    Console.WriteLine($"Monster was damaged and lost {roll} health and now has {villianHealth} health.");
-    
-    if(villianHealth <= 0)continue;
+    readResult = Console.ReadLine();
+} while (readResult == null);
+int numericValue = 0;
+bool validNumber = false;
 
-    roll = dice.Next(1,11);
-    heroeHealth -= roll;
-    Console.WriteLine($"Hero was damaged and lost {roll} health and now has {heroeHealth} health.");
-} while (heroeHealth > 0 && villianHealth > 0);
-Console.WriteLine(heroeHealth > villianHealth ? "Hero Wins!" : "Monster Wins!");
+validNumber = int.TryParse(readResult, out numericValue);
