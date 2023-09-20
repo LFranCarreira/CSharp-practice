fc5e1d78e6c99ad5e8d99df8fd60c16090be8047
Franco Carreira <carreirafranco@gmail.com>
2023-09-20 16:52:59 -0300

    new Excersice

--- CsharpProjects/ChangeValues/Program.cs
+++ CsharpProjects/ChangeValues/Program.cs
@@ -81,22 +81,36 @@
 //     Console.WriteLine($"Measurement (w/ offset): {50 + result}");
 
 
+// string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+// decimal total = 0m;
+// string message = "";
+// foreach (var val in values)
+// {
+//     decimal number;
+//     if (decimal.TryParse(val, out number))
+//     {
+//         total += number;
+//     }
+//     else
+//     {
+//         message += val;
+//     }
 
-string[] values = { "12.3", "45", "ABC", "11", "DEF" };
-decimal total = 0m;
-string message = "";
-foreach (var val in values)
-{
-    decimal number;
-    if (decimal.TryParse(val, out number))
-    {
-        total += number;
-    }
-    else
-    {
-        message += val;
-    }
+// }
+// Console.WriteLine($"Message: {message}");
+// Console.WriteLine($"Total: {total}");
 
-}
-Console.WriteLine($"Message: {message}");
-Console.WriteLine($"Total: {total}");
+
+int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+int result1 = Convert.ToInt32((decimal)value1 / value2);
+// Hint: You need to round the result to nearest integer (don't just truncate)
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
