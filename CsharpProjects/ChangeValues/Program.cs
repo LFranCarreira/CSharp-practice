0f1628781799e67bb9e9b060a649521180b66993
Franco Carreira <carreirafranco@gmail.com>
2023-09-20 16:13:26 -0300

    value out

--- CsharpProjects/ChangeValues/Program.cs
+++ CsharpProjects/ChangeValues/Program.cs
@@ -41,7 +41,20 @@
 // int value2 = Convert.ToInt32(1.5m); // converting rounds up
 // Console.WriteLine(value2);
 
-string value = "102";
+// string value = "102";
+// int result = 0;
+// if (int.TryParse(value, out result))
+// {
+//     Console.WriteLine($"Measurement: {result}");
+// }
+// else
+// {
+//     Console.WriteLine("Unable to report the measurement.");
+// }
+
+// Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+
+string value = "bad";
 int result = 0;
 if (int.TryParse(value, out result))
 {
@@ -51,3 +64,6 @@ else
 {
     Console.WriteLine("Unable to report the measurement.");
 }
+
+if (result < 0)
+    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
