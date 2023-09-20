6e71821de8baa3e0aafd6f1d0b6319a19e46b3c2
Franco Carreira <carreirafranco@gmail.com>
2023-09-20 16:42:21 -0300

    simple challenge

--- CsharpProjects/ChangeValues/Program.cs
+++ CsharpProjects/ChangeValues/Program.cs
@@ -5,41 +5,52 @@
 // int myInt = 3;
 // Console.WriteLine($"int: {myInt}");
 
+
 // decimal myDecimal = myInt;
 // Console.WriteLine($"decimal: {myDecimal}");
+
 
 // decimal myDecimal = 3.14m;
 // Console.WriteLine($"decimal: {myDecimal}");
 
+
 // int myInt = (int)myDecimal;
 // Console.WriteLine($"int: {myInt}");
+
 
 // decimal myDecimal = 1.23456789m;
 // float myFloat = (float)myDecimal;
 
+
 // Console.WriteLine($"Decimal: {myDecimal}");
 // Console.WriteLine($"Float  : {myFloat}");
+
 
 // int first = 5;
 // int second = 7;
 // string message = first.ToString() + second.ToString();
 // Console.WriteLine(message);
 
+
 // string first = "5";
 // string second = "7";
 // int sum = int.Parse(first) + int.Parse(second);
 // Console.WriteLine(sum);
+
 
 // string value1 = "5";
 // string value2 = "7";
 // int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
 // Console.WriteLine(result);
 
+
 // int value = (int)1.5m; // casting truncates
 // Console.WriteLine(value);
 
+
 // int value2 = Convert.ToInt32(1.5m); // converting rounds up
 // Console.WriteLine(value2);
+
 
 // string value = "102";
 // int result = 0;
@@ -54,16 +65,38 @@
 
 // Console.WriteLine($"Measurement (w/ offset): {50 + result}");
 
-string value = "bad";
-int result = 0;
-if (int.TryParse(value, out result))
-{
-    Console.WriteLine($"Measurement: {result}");
-}
-else
-{
-    Console.WriteLine("Unable to report the measurement.");
-}
 
-if (result < 0)
-    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+// string value = "bad";
+// int result = 0;
+// if (int.TryParse(value, out result))
+// {
+//     Console.WriteLine($"Measurement: {result}");
+// }
+// else
+// {
+//     Console.WriteLine("Unable to report the measurement.");
+// }
+
+// if (result < 0)
+//     Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+
+
+
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+decimal total = 0m;
+string message = "";
+foreach (var val in values)
+{
+    decimal number;
+    if (decimal.TryParse(val, out number))
+    {
+        total += number;
+    }
+    else
+    {
+        message += val;
+    }
+
+}
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}");
