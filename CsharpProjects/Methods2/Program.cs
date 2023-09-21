32c4306aa90c5a289fbe446684f01b6a102d45d5
Franco Carreira <carreirafranco@gmail.com>
2023-09-21 18:05:55 -0300

    LastIndexOf()

--- CsharpProjects/Methods2/Program.cs
+++ CsharpProjects/Methods2/Program.cs
@@ -1,10 +1,31 @@
-﻿string message = "Find what is (inside the parentheses)";
+﻿// string message = "Find what is (inside the parentheses)";
 
-int openingPosition = message.IndexOf('(');
-int closingPosition = message.IndexOf(')');
+// int openingPosition = message.IndexOf('(');
+// int closingPosition = message.IndexOf(')');
 
-// Console.WriteLine(openingPosition);
-// Console.WriteLine(closingPosition);
+// openingPosition += 1;
 
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition, length));
+
+
+// string message = "What is the value <span>between the tags</span>?";
+
+// const string openSpan = "<span>";
+// const string closeSpan = "</span>";
+
+// int openingPosition = message.IndexOf(openSpan);
+// int closingPosition = message.IndexOf(closeSpan);
+
+// openingPosition += openSpan.Length;
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition, length));
+
+
+string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+int openingPosition = message.LastIndexOf('(');
+
+openingPosition += 1;
+int closingPosition = message.LastIndexOf(')');
 int length = closingPosition - openingPosition;
 Console.WriteLine(message.Substring(openingPosition, length));
