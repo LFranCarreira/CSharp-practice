e27ee872d6ab383c8bcc08bd65eb73adc4ccd48f
Franco Carreira <carreirafranco@gmail.com>
2023-09-22 19:20:30 -0300

    first method

--- CsharpProjects/MethodCreation/Program.cs
+++ CsharpProjects/MethodCreation/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Before calling a method");
+SayHello();
+Console.WriteLine("After calling a method");
+
+void SayHello() 
+{
+    Console.WriteLine("Hello World!");
+}
+
+
+int[] a = {1,2,3,4,5};
+
+Console.WriteLine("Contents of Array:");
+PrintArray();
+
+void PrintArray()
+{
+    foreach (int x in a)
+    {
+        Console.Write($"{x} ");
+    }
+    Console.WriteLine();
+}
