18c9dad451fa9e5e6845618b71ad9b23aecbeb9e
Franco Carreira <carreirafranco@gmail.com>
2023-09-23 10:38:01 -0300

    finished

--- CsharpProjects/MethodCreation/Program.cs
+++ CsharpProjects/MethodCreation/Program.cs
@@ -122,18 +122,18 @@ then ipAddress is valid
 
 else ipAddress is invalid
 */
-string ipv4Input = "107.31.1.5";
+string[] ipv4Input = {"107.31.1.5", "255.0.0.255", "555..0.555", "255...255"};
+string[] address;
 bool validLength = false;
 bool validZeroes = false;
 bool validRange = false;
+
 void ValidateLength() 
 {
-    string[] address = ipv4Input.Split(".");
     validLength = address.Length == 4;
 };
 void ValidateZeroes() 
 {
-    string[] address = ipv4Input.Split(".");
 
     foreach (string number in address) 
     {
@@ -148,7 +148,6 @@ void ValidateZeroes()
 }
 void ValidateRange() 
 {
-    string[] address = ipv4Input.Split(".", StringSplitOptions.RemoveEmptyEntries);
 
     foreach (string number in address) 
     {
@@ -160,15 +159,19 @@ void ValidateRange()
     }
     validRange = true;
 }
-ValidateLength(); 
-ValidateZeroes(); 
-ValidateRange();
+foreach (string ip in ipv4Input) 
+{
+    address = ip.Split(".", StringSplitOptions.RemoveEmptyEntries);
+    ValidateLength(); 
+    ValidateZeroes(); 
+    ValidateRange();
 
-if (validLength && validZeroes && validRange) 
-{
-    Console.WriteLine($"ip is a valid IPv4 address");
-} 
-else
-{
-    Console.WriteLine($"ip is an invalid IPv4 address");
+    if (validLength && validZeroes && validRange) 
+    {
+        Console.WriteLine($"{ip} is a valid IPv4 address");
+    } 
+    else 
+    {
+        Console.WriteLine($"{ip} is an invalid IPv4 address");
+    }
 }
