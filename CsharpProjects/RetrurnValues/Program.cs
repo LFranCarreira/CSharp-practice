5ff9edca3ec3d5b42f3eec0ce8c164bd02ac9a09
Franco Carreira <carreirafranco@gmail.com>
2023-09-24 01:23:51 -0300

    reverse

--- CsharpProjects/RetrurnValues/Program.cs
+++ CsharpProjects/RetrurnValues/Program.cs
@@ -51,10 +51,22 @@
 // }
 
 
-string input = "snake";
+string input = "there are snakes at the zoo";
 
 Console.WriteLine(input);
-Console.WriteLine(ReverseWord(input));
+Console.WriteLine(ReverseSentence(input));
+
+string ReverseSentence(string input)
+{
+    string result = "";
+    string[] words = input.Split(" ");
+    foreach (string word in words)
+    {
+        result += ReverseWord(word) + " ";
+    }
+    return result.Trim();
+}
+
 string ReverseWord(string word)
 {
     string result = "";
